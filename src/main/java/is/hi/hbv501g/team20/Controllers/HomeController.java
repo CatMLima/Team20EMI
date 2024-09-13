0eacb7ca24058d23a166ce9581c35a0607c92b6e
Catarina (Cat) Lima <catisousalima1999@gmail.com>
2024-09-13 20:45:56 +0000

    Initial commit

---
+++ src/main/java/is/hi/hbv501g/team20/Controllers/HomeController.java
@@ -0,0 +1,16 @@
+package is.hi.hbv501g.team20.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String HomeController(){
+        // Business logic
+        // Call a method in a service class
+        // Add some data to the model
+           return "home";
+    }
+}
